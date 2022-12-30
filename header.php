a36d6d5613ee2cf6b5af4afd964b93f5d6f3c4f0
SimonPietrucha <121490332+SimonPietrucha@users.noreply.github.com>
2022-12-30 14:33:46 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <link rel="stylesheet" href="home.css">
+</head>
+<body>
+  <header>
+    <div class="company-logo">D</div>
+    <nav class="navbar">
+      <ul class="nav-items">
+        <li class="nav-item"><a href="index.html" class="nav-link">Startseite</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Ausleihen</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Kontakt</a></li>
+        <li class="nav-item"><a href="login.html" class="nav-link">Login</a></li>
+      </ul>
+    </nav>
+    <div class="menu-toggle">
+      <i class="bx bx-menu"></i>
+      <i class="bx bx-x"></i>
+    </div>
+  </header>
+  <main>
+    <!-- HOME SECTION -->
+    <section class="container section-1">
+      <div class="slogan">
+        <h1 class="company-title">Roller-Verleih</h1>
+        <h2 class="company-slogan">
+          Mit uns kommen sie immer ans Ziel
+        </h2>
+      </div>
+      <div class="home-computer-container">
+        <img class="home-computer" src="https://github.com/r-e-d-ant/Computer-store-website/blob/main/assets/images/home_img.png?raw=true" alt="a computer in dark with shadow" class="home-img">
+      </div>
+    </section>
---
+++ tail.php
@@ -0,0 +1,51 @@
+<footer>
+    <div class="container top-footer">
+      <!-- footer item 1 -->
+      <div class="footer-item">
+        <h2 class="footer-title">ADDRESS</h2>
+        <div class="footer-items">
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing st18</h3>
+        </div>
+      </div>
+      <!-- footer item 2 -->
+      <div class="footer-item">
+        <h2 class="footer-title">SERVICES</h2>
+        <div class="footer-items">
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+        </div>
+      </div>
+      <!-- footer item 3 -->
+      <div class="footer-item">
+        <h2 class="footer-title">SUPPLIERS</h2>
+        <div class="footer-items">
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+        </div>
+      </div>
+      <!-- footer item 4 -->
+      <div class="footer-item">
+        <h2 class="footer-title">INVESTMENT</h2>
+        <div class="footer-items">
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+          <h3>Adipisicing elit.</h3>
+        </div>
+      </div>
+    </div>
+    <div class="container end-footer">
+      <div class="copyright">copyright © 2021 - Present • <b>DOWNTOWN TECH LIVER</b></div>
+      <a class="designer" href="#">Thierry M</a>
+    </div>
+  </footer>
+  </body>
