23316bc0ad360a16a8269b46e58fbf6a43871cdc
MartinMaierr <121491123+MartinMaierr@users.noreply.github.com>
2023-01-13 23:59:09 +0100

    Update ausleihen.php

--- ausleihen.php
+++ ausleihen.php
@@ -4,26 +4,16 @@
 
     <!-- OFFER SECTION -->
     <section class="container section-2">
-
         <!-- <div id="map"></div>
-
-        <script>
-            function initMap() {
-                var location = {lat: 49.488888, lng: 8.469167};
-                var map = new google.maps.Map(document.getElementById("map"), {
-                    zoom: 4,
-                    center: location
-                });
-                var marker = new google.maps.Marker({
-                    position: location,
-                    map: map
-                });
-            }
-        </script>
-        <script async defer src="https://maps.googleapis.com/maps/apijs?key=...&callback=initMap"></script> -->
-
       <div>
-        <iframe src="https://www.google.com/maps/d/embed?mid=19TLK5nSy9dgKegXmrozdXHoDv78VV-I&ehbc=2E312F" width="640" height="480"></iframe>
+        <iframe src="https://my.atlistmaps.com/map/be2a8bcf-2bd7-49c0-8ded-dbe7647eec9b?share=true" 
+            allow="geolocation" 
+            width="100%" 
+            height="400px" 
+            frameborder="0" 
+            scrolling="no" 
+            allowfullscreen>
+        </iframe>      
       </div>
     </section>
 
