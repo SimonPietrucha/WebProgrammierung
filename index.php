3f1276d3e16f2c9b12cd85b22c3557513faf0c41
SimonPietrucha <121490332+SimonPietrucha@users.noreply.github.com>
2023-01-20 11:49:30 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php 
       require("templates/header.php");
 ?>
-    <!-- OFFER SECTION -->
-    <!-- <section class="container section-2">
-      
-      <div class="offer offer-1">
-        <img src="img/EScooterVonVorne.jpg" alt="E-Scooter" class="offer-img offer-1-img">
-        <div class="offer-description offer-desc-1">
-          <h2 class="offer-title">E-Scooter</h2>
-          <p class="offer-hook">Kommen sie mit unseren Top modernen E-Scootern richtig in Fahrt!</p>
-          <div class="cart-btn" >
-           <a href="ausleihen.php"> Starte deine Tour </a>
-
-          </div>
-        </div>
-      </div>
-      
-      <div class="offer offer-2">
-        <div class="offer-description offer-desc-2">
-          <h2 class="offer-title">E-Bike</h2>
-          <img src="img/EBike.jpg" alt="E-Bike" class="offer-img offer-2-img">
-          <p class="offer-hook">Mit unseren E-Bikes sind sie Perfekt für den Stadtverkehr ausgestattet!</p>
-          <div class="cart-btn">
-            <a href="ausleihen.php"> Starte deine Tour </a>
-          </div>
-        </div>
-      </div>
-    </section> -->
     <section>
     <div class="slideshow">
         <div class="slide">
