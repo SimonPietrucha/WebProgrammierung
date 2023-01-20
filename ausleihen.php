6e9861e23285bd4d98d2f1e7307110246ba4fe23
MartinMaierr <121491123+MartinMaierr@users.noreply.github.com>
2023-01-20 11:19:18 +0100

    Update ausleihen.php

--- ausleihen.php
+++ ausleihen.php
@@ -16,11 +16,11 @@
         </iframe>      
       </div>
 
-        <!--<div id="map"></div>
+        <div id="map"></div>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
         <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
 
-        <script src="map.js"></script> -->
+        <script src="map.js"></script>
 
     </section>
 
