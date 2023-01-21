7a6c1af626cb58906f798869a7750839f989a7fc
SimonPietrucha <121490332+SimonPietrucha@users.noreply.github.com>
2023-01-21 22:31:07 +0100

    Add files via upload

--- ausleihen.php
+++ ausleihen.php
@@ -1,28 +1,29 @@
 <?php 
       require("templates/header.php");
 ?>
+        <!-- <iframe id = "map" width="625" height="550" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+        src="https://www.openstreetmap.org/export/embed.html?bbox=8.430976867675783%2C49.47944587036476%2C8.481273651123049%2C49.495559587548776&amp;layer=mapnik">
+        </iframe><br/>
+        <small>
+            <a href="https://www.openstreetmap.org/#map=15/49.4875/8.4561">Größere Karte anzeigen
 
-    <!-- OFFER SECTION -->
-    <section class="container section-2">
-        <!-- <div id="map"></div>
-      <div>
-        <iframe src="https://my.atlistmaps.com/map/be2a8bcf-2bd7-49c0-8ded-dbe7647eec9b?share=true" 
-            allow="geolocation" 
-            width="100%" 
-            height="400px" 
-            frameborder="0" 
-            scrolling="no" 
-            allowfullscreen>
-        </iframe>      
-      </div>
-
-        <div id="map"></div>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-
-        <script src="map.js"></script>
-
-    </section>
+            </a>
+        </small> -->
+ 
+      <link rel="stylesheet" href="leaflet.css" />
+      <script src="leaflet.js"></script>
+      <div id="map" ></div>
+      <script src="map.js"></script>
+    
+    <div class="box">
+        <a class="ausleihKnopf" href="ausleihen.php"> Ausleihen </a>
+            <select>
+        <option>E-Scooter 1</option>
+        <option>E-Scooter 2</option>
+        <option>E-Bike 1</option>
+        <option>E-Bike 2</option>
+      </select>
+    </div>
 
 <?php
     require("templates/tail.php");
